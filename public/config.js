79b0c6ad19fbb8426e6ef07c7b386c8e14e3b451
bxm <baixmin@gmail.com>
2023-06-09 20:40:46 +0800

    first commit

---
+++ public/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '慕斯 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2168156-1144641e58a1e3e9ed1b0361',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.dd.ac.cn/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.dd.ac.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/'
+    },
+  ],
+};
